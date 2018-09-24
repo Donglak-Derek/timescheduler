3f086426acc671e8e0e1bac0a2f39b74f5163cf7
Derek_Han <donglak.han@gmail.com>
2018-09-24 14:09:14 -0500

    Added status for posts with Enum

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Kim")
+puts "1 admin user created!"
+
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Derek", last_name: "Han")
-puts "One user created!"
+puts "1 user created!"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
