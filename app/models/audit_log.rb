1f046d45fb13c701200e25de5d2489f1a6e06a50
Derek_Han <donglak.han@gmail.com>
2018-10-01 14:07:22 -0500

    intergrated application helper for status labels and intergrated enums for audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,12 +1,14 @@
 class AuditLog < ApplicationRecord
-  belongs_to :user
+	enum status: { pending: 0, confirmed: 1}
 
-  validates_presence_of :user_id, :status, :start_date
+	belongs_to :user
 
-  after_initialize :set_defaults
+	validates_presence_of :user_id, :status, :start_date
 
-  private
-  	def set_defaults
-  		self.start_date ||= Date.today - 6.days
-  	end
+	after_initialize :set_defaults
+
+	private
+		def set_defaults
+			self.start_date ||= Date.today - 6.days
+		end
 end
