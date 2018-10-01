c1548f7d9faeb5113e25c030942cf4adbb50c0ce
Derek_Han <donglak.han@gmail.com>
2018-10-01 13:36:22 -0500

    Blocked non admin users from accessing index page for audits

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,7 +1,8 @@
 class AuditLogsController < ApplicationController
 	def index
+		# lines order!
 		@audit_logs = AuditLog.all
+		authorize @audit_logs
 	end
-
 
 end
--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -20,8 +20,14 @@ describe 'AuditLog Feature' do
 			expect(page).to have_content(/DEREK, HAN/)
 		end
 
-		xit 'cannot be accessed by non admin user' do
+		it 'cannot be accessed by non admin user' do
+			logout(:user)
+			user = FactoryBot.create(:user)
+			login_as(user, :scope => :user)
 
+			visit audit_logs_path
+
+			expect(current_path).to eq(root_path)
 		end
 	end
 end
