56800602646f61053680aed5099d9c64038682b7
Derek_Han <donglak.han@gmail.com>
2018-10-02 11:28:58 -0500

    Styled the employee homepage pending audit approvals

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "1 admin user created!"
 					)
 puts "1 user created!"
 
-100.times do |post|
+54.times do |post|
 	Post.create!(
 					date: Date.today, 
 					rationale: "#{post} It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
@@ -26,15 +26,16 @@ puts "1 user created!"
 					overtime_request: 2.5,
 				)
 end
+puts "54 Posts have been created"
 
-puts "100 Posts have been created"
+AuditLog.create!( user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!( user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!( user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+puts "3 audit logs have been created!"
 
-100.times do |audit_log|
-	AuditLog.create!(
-					user_id: @user.id, 
-					status: 0, 
-					start_date: Date.today - 6.days,
-				)
-end
 
-puts "100 AuditLog have been created"
+
+
+
+
+
