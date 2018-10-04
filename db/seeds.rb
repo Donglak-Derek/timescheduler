e94503aae875621c79d3ccf872f9c648e51b4eb5
Derek_Han <donglak.han@gmail.com>
2018-10-04 08:34:58 -0500

    Changed ruby version?

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ AuditLog.create!( user_id: @user.id, status: 0, start_date: (Date.today - 13.day
 AuditLog.create!( user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 puts "3 audit logs have been created!"
 
-54.times do |post|
+100.times do |post|
 	Post.create!(
 					date: Date.today, 
 					rationale: "#{post} It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", 
@@ -31,7 +31,7 @@ puts "3 audit logs have been created!"
 					overtime_request: 2.5,
 				)
 end
-puts "54 Posts have been created"
+puts "100 Posts have been created"
 
 
 
